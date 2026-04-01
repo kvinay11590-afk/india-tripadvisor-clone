908e1f417f83bf93f49b2ae0312a98c0729673e0
kvinay11590-afk <kvinay11590@gmail.com>
2026-04-01 11:02:18 +0530

    Add tourism board scraper module

---
+++ app/scrapers/tourism_board_scraper.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrape_incredible_india():
+    # Your code to scrape Incredible India website
+    pass
+
+
+def scrape_state_tourism_boards():
+    # Your code to scrape various state tourism boards
+    pass
